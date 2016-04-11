e678852f7b15b093b7c46b38b2c842584ec84f37
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-04-11 07:29:20 -0300

    using middleware syntax

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	m "github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/engine/fasthttp"
 )
 
 func main() {
 	e := echo.New()
 
+	e.Use(m.Static(""))
+	e.Use(m.Static("client/dev"))	
+
 	e.Get("/api", func(c echo.Context) error {
 		return c.String(200, "1")
 	})
-
-	e.Get("/", echo.Static("client/dev"))
-	e.Get("/*", echo.Static(""))
 
 	e.Run(fasthttp.New(":3000"))
 }
